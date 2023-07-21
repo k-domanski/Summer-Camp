482b50b15e823684ddd69eb6f6aff1d15518aa87
Karthag <kd.domanski@gmail.com>
2023-07-21 23:46:26 +0200

    fixed merge issues.

--- SummerCamp/Assets/Scripts/FinishLine.cs
+++ SummerCamp/Assets/Scripts/FinishLine.cs
@@ -6,28 +6,10 @@ using Elympics;
 
 public class FinishLine : ElympicsMonoBehaviour
 {
-    public ElympicsFloat p1Time = new ElympicsFloat();
-    public ElympicsFloat p2Time = new ElympicsFloat();
-
-
-    [SerializeField] private PlayersProvider playersProvider;
-    [SerializeField] private RaceTimer timer;
-
+    [SerializeField] PlayersProvider playersProvider;
     public event Action<int> onPlayerFinished;
-    public event Action<List<ElympicsFloat>, List<ElympicsInt>> onScoreUpdated;
-
-    public ElympicsBool scoreUpdate = new ElympicsBool();
-
-    private static List<ElympicsFloat> playersTime = new List<ElympicsFloat>();
-    private static List<ElympicsInt> playersID = new List<ElympicsInt>();
-
+    
     private bool reachedFinish;
-    private void Start() => scoreUpdate.ValueChanged += HandleScoreUpdate;
-
-    private void HandleScoreUpdate(bool lastValue, bool newValue)
-    {
-        //onScoreUpdated?.Invoke(playersTime, playersID);
-    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -38,15 +20,9 @@ public class FinishLine : ElympicsMonoBehaviour
         {
             if (!reachedFinish)
             {
-                p1Time.Value = timer.CurrentTime.Value;
-
                 reachedFinish = playersProvider.ClientPlayer.PlayerID == playerData.PlayerID;
-                //playersTime.Add(new ElympicsFloat(timer.CurrentTime.Value));
-                //onPlayerFinished?.Invoke(playerData.PlayerID, new ElympicsFloat(timer.CurrentTime.Value));
-                scoreUpdate.Value = true;
-
+                onPlayerFinished?.Invoke(playerData.PlayerID);
             }
-            onPlayerFinished?.Invoke(playerData.PlayerID);
 
             //robocze
             playerData.GetComponent<InputProvider>().DisableInput();
--- SummerCamp/Assets/Scripts/GUI/ScoreBoardUI.cs
+++ SummerCamp/Assets/Scripts/GUI/ScoreBoardUI.cs
@@ -9,10 +9,8 @@ public class ScoreBoardUI : MonoBehaviour
     [SerializeField] private List<PlayerScorePanelUI> playerScorePanels;
     [SerializeField] private FinishLine finishLine;
     [SerializeField] private PlayersProvider playersProvider;
-    [SerializeField] private RaceTimer timer;
 
     private static int playerCount = 0;
-    private ElympicsInt panelIndex = new ElympicsInt(0);
 
     private void Start()
     {
@@ -35,62 +33,17 @@ public class ScoreBoardUI : MonoBehaviour
     private void OnEnable()
     {
         finishLine.onPlayerFinished += ShowScoreboard;
-        finishLine.scoreUpdate.ValueChanged += TestTime;
-        //finishLine.onPlayerFinished += HandleScore;
-        //finishLine.p1Time.ValueChanged += HandleP1Score;
-        //finishLine.p2Time.ValueChanged += HandleP2Score;
-        //finishLine.onScoreUpdated += UpdateScore;
-    }
-
-    private void TestTime(bool lastValue, bool newValue)
-    {
-        playerScorePanels[0].SetPlayerScoreText(12, timer.CurrentTime.Value);
-    }
-
-    private void HandleP2Score(float lastValue, float newValue)
-    {
-        ShowScoreboard(1);
-        playerScorePanels[1].SetPlayerScoreText(1, newValue);
-    }
-
-    private void HandleP1Score(float lastValue, float newValue)
-    {
-        ShowScoreboard(0);
-        playerScorePanels[0].SetPlayerScoreText(0, newValue);
-    }
-
-    private void UpdateScore(List<ElympicsFloat> arg1, List<ElympicsInt> arg2)
-    {
-        for(int i = 0; i < arg1.Count; i++)
-        {
-            Debug.Log($"TIME: {arg1[i].Value} | ");
-            playerScorePanels[i].SetPlayerScoreText(arg2[i].Value, arg1[i].Value);
-            ShowScoreboard(arg2[i].Value);
-
-        }
     }
 
     private void OnDisable()
     {
-        //finishLine.onPlayerFinished -= HandleScore;
+        finishLine.onPlayerFinished += ShowScoreboard;
         playersProvider.IsReadyChanged -= GetPlayerCount;
     }
 
     private void GetPlayerCount()
     {
         playerCount = playersProvider.AllPlayers.Length;
-    }
-
-    private void HandleScore(int playerID, ElympicsFloat time)
-    {
-            ShowScoreboard(playerID);
-            SetPlayerScore(playerID, time);
-            panelIndex.Value++;
-    }
-
-    private void SetPlayerScore(int playerID, ElympicsFloat time)
-    {
-        playerScorePanels[panelIndex.Value].SetPlayerScoreText(playerID, time.Value);
     }
 
     private void ShowScoreboard(int playerID)
@@ -103,7 +56,5 @@ public class ScoreBoardUI : MonoBehaviour
                 playerScorePanels[i].ShowPanel(true);
             }
         }
-        
-
     }
 }
