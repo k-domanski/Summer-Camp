a46bcf4cd94bc8f486344881d750016069826282
Karthag <kd.domanski@gmail.com>
2023-07-23 16:42:35 +0200

    Added MainMenu scene in order to play via cloud.

---
+++ SummerCamp/Assets/Scripts/MatchmakingManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Elympics;
+using UnityEngine.UI;
+
+public class MatchmakingManager : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+
+    private void Awake()
+    {
+        startButton.onClick.AddListener(PlayOnline);
+    }
+
+    private void OnDestroy()
+    {
+        startButton.onClick.RemoveListener(PlayOnline);
+    }
+
+    public void PlayOnline()
+    {
+        //ElympicsLobbyClient.Instance.PlayOnlineInRegion(null, null, null, "Default");
+        Debug.Log("Button Pressed");
+        startButton.interactable = false;
+    }
+}
