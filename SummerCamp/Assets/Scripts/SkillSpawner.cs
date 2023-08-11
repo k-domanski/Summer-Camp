16ddf7b8cc6f219f8f125f589c59af6b03f568dd
Adelbart <adelbartmail@gmail.com>
2023-08-11 14:07:48 +0200

    Prefab for power ups spawner

---
+++ SummerCamp/Assets/Scripts/SkillSpawner.cs
@@ -0,0 +1,60 @@
+using System;
+using Elympics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillSpawner : ElympicsMonoBehaviour, IUpdatable
+{
+    [SerializeField]
+    private GameManager manager;
+    [SerializeField]
+    private Image spawnIndicator;
+    [SerializeField]
+    private GameObject spawn;
+    [SerializeField]
+    private float cooldownTime;
+
+    public ElympicsFloat TimeToSpawn { get; private set; } = new ElympicsFloat();
+
+    private void Start()
+    {
+        if (Elympics.IsServer)
+        {
+            TimeToSpawn.Value = cooldownTime;
+        }
+        if (Elympics.IsClient)
+        {
+            TimeToSpawn.ValueChanged += OnTimeChanged;
+        }
+    }
+
+    private void OnTimeChanged(float lastvalue, float newvalue)
+    {
+        spawnIndicator.fillAmount = 1 - (newvalue / cooldownTime);
+    }
+
+    public void ElympicsUpdate()
+    {
+        if (manager.IsRunning.Value == false)
+        {
+            return;
+        }
+        
+        if (Elympics.IsServer)
+        {
+            if (spawn.activeInHierarchy == false)
+            {
+                TimeToSpawn.Value -= Elympics.TickDuration;
+                if (TimeToSpawn.Value <= 0)
+                {
+                    spawn.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void Pick()
+    {
+        TimeToSpawn.Value = cooldownTime;
+    }
+}
