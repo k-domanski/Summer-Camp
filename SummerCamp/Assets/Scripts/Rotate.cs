ba18a2d94640a294046a0bad99863ad48abf3d51
Karthag <kd.domanski@gmail.com>
2023-08-14 19:28:40 +0200

    changed obstacle rotation, added fan obstacle.

---
+++ SummerCamp/Assets/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Elympics;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private Vector3 rotationAxis;
+
+    private void Update()
+    {
+        transform.Rotate(rotationAxis, Time.deltaTime * speed);
+
+    }
+}
