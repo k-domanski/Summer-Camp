ce32c65aa9076316f380ab7e78ae79ad14b9d34a
Karthag <kd.domanski@gmail.com>
2023-09-05 15:47:03 +0200

    bomb explosion.

--- SummerCamp/Assets/Scripts/Skill/Bomb.cs
+++ SummerCamp/Assets/Scripts/Skill/Bomb.cs
@@ -8,14 +8,27 @@ public class Bomb : ElympicsMonoBehaviour, IUpdatable, IRemovable
     [SerializeField] private float range;
     [SerializeField] private float timeToBoom;
     [SerializeField] private float force = 100.0f;
+    [SerializeField] private float timeToDestroy = 2.0f;
+
+    [Header("References")]
+    [SerializeField] private GameObject bombModel;
+    [SerializeField] private ParticleSystem explosionParticles;
 
     public ElympicsFloat currentBombTime = new ElympicsFloat();
+    public ElympicsFloat currentTimeToDestroy = new ElympicsFloat();
+
     public ElympicsBool markedToDestroy = new ElympicsBool();
 
     public void ElympicsUpdate()
     {
         if (markedToDestroy.Value)
-            ElympicsDestroy(this.gameObject);
+        {
+            currentTimeToDestroy.Value += Elympics.TickDuration;
+
+            if(currentBombTime.Value >= timeToDestroy)
+                ElympicsDestroy(this.gameObject);
+
+        }
 
         currentBombTime.Value += Elympics.TickDuration;
 
@@ -28,6 +41,7 @@ public class Bomb : ElympicsMonoBehaviour, IUpdatable, IRemovable
     public void Remove()
     {
         markedToDestroy.Value = true;
+        currentTimeToDestroy.Value = timeToDestroy;
     }
 
     private void Explode()
@@ -43,7 +57,10 @@ public class Bomb : ElympicsMonoBehaviour, IUpdatable, IRemovable
                 //player.GetComponent<Rigidbody>().AddExplosionForce(force, transform.position, range, 0.0f, ForceMode.Impulse);
             }
         }
+        bombModel.SetActive(false);
+        explosionParticles.Play();
         markedToDestroy.Value = true;
         currentBombTime.Value = 0.0f;
+        currentTimeToDestroy.Value = 0.0f;
     }
 }
